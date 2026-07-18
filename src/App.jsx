065a9a577aed30cb83cfd294aac6887423c04ceb
Hadiuzzaman <zhhadi50@gmail.com>
2026-07-18 12:27:11 +0600

    little more suger

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import { useState } from 'react';
 
 const App = () => {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
 
   async function getAdvice() {
     const res = await fetch('https://api.adviceslip.com/advice');
     const data = await res.json();
+    console.log(data.slip)
     setAdvice(data.slip.advice);
+    setCount(c=>c + 1);
   }
 
   useEffect(()=> {
@@ -19,8 +22,13 @@ const App = () => {
     <div>
       <h2>{advice}</h2>
       <button onClick={getAdvice}>Get Advice</button>
+      <Message count={count} />
     </div>
   )
+}
+
+function Message({count}){
+  return <p>You have read {count} pieces of advice.</p>
 }
 
 export default App
